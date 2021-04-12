474b0aae99066dee411c1cadd977c170351cff44
George Camboim <george.camboim@embedded.ufcg.edu.br>
2021-04-11 22:48:33 -0300

    Adicionando whitenoise e mudando configuração default para heroku.py

--- django_backend/settings/_base.py
+++ django_backend/settings/_base.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-ALLOWED_HOSTS = ['127.0.0.1', 'colheita-feliz.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
@@ -100,4 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
