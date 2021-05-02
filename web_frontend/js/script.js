f4edd673be672db7bac7fc6bfb36e6f8cddb50be
marcio <marcio.almeida@embedded.ufcg.edu.br>
2021-05-02 16:07:58 -0300

    Site basicamente funcional, falta deixar bonito, destruir os gráficos e desenvolver um sistema de login

--- web_frontend/js/script.js
+++ web_frontend/js/script.js
@@ -1,19 +1,15 @@
-'use strict';
 
-fetch("./first.json")
-    .then(function(resp){
-        return resp.json();
-    })
-    .then(function(data){
+const fucki= function (work){
+
+const req= new XMLHttpRequest();
+    req.open('GET','https://colheita-feliz.herokuapp.com/api/get/status/1/day/');
+    req.send();
+
+    req.addEventListener('load',function(){
+        
+        const data= JSON.parse(req.responseText)
         console.log(data);
-    });
+        
 
-
-/*const request = new XMLHttpRequest();
-    request.open('GET','https://colheita-feliz.herokuapp.com/api/get/status/1/week/');
-    request.send();
-    console.log(request.responseText);
-    
-    request.addEventListener('load',function(){
-        console.log(this.responseText);
-    })*/
+    })
+}
